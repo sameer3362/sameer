ed9a509a7c9e830fed539e3ab3bff4c2e7894b19
samshivane <sameer.shivane3362@gmail.com>
2020-03-23 21:21:12 +0530

    added files

---
+++ src/ClassOneProject.java
@@ -0,0 +1,26 @@
+
+public class ClassOneProject {
+
+	public static void main(String[] args) {
+
+		System.out.println("Main Method executed");
+		method();
+		method2();
+		method3();
+	}
+
+	public static void method() {
+		System.out.println("Method One");
+	}
+
+	public static void method2() {
+		System.out.println("Method Two");
+
+	}
+
+	public static void method3() {
+		System.out.println("Method Three");
+
+	}
+
+}
